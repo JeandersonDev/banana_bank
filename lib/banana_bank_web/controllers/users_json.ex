9d50d305789840912bbd614fdb34cbca0b04c592
Jeanderson Almeida <jeanderson.2315@gmail.com>
2023-07-20 13:58:18 -0300

    Creating fallback, and get user

--- lib/banana_bank_web/controllers/users_json.ex
+++ lib/banana_bank_web/controllers/users_json.ex
@@ -8,6 +8,8 @@ defmodule BananaBankWeb.UsersJSON do
     }
   end
 
+  def get(%{user: user}), do: %{data: data(user)}
+
   defp data(%User{id: id, name: name, email: email, cep: cep}) do
     %{
       id: id,
